5720493878a94a568bc9dceaddcf53c3a9d73f04
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-10-18 09:59:02 +0200

    test

--- server/public/super_secret_spy_code.js
+++ server/public/super_secret_spy_code.js
@@ -38,7 +38,7 @@ self.addEventListener('fetch', function(event) {
         var responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
+          cache.post(event.request, responseToCache);
         });
 
         return response;
