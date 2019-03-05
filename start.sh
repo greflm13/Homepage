9b575cde706d539072196938aea9a145f65fffd4
greflm13 <greflm13@htl-kaindorf.ac.at>
2019-03-05 15:12:51 +0100

    variable host

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo enter Hostname:
+read HOST
+cd chat && scp -r dist/* $HOST:/home/pi/server &&
+cd ../discord && scp -r dist/* $HOST:/home/pi/server &&
+cd ../films && scp -r dist/* $HOST:/home/pi/server &&
+cd ../game2048 && scp -r dist/* $HOST:/home/pi/server &&
+cd ../minecraft-server && scp -r dist/* $HOST:/home/pi/server &&
+cd ../minesweeper && scp -r dist/* $HOST:/home/pi/server &&
+cd ../server && scp -r dist/* $HOST:/home/pi/server &&
+cd ../timeline && scp -r dist/* $HOST:/home/pi/server
