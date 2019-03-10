36b5e41247c633724748627b461f423a8443fe4e
greflm13 <greflm13@htl-kaindorf.ac.at>
2019-03-10 23:30:09 +0100

    log fix

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo enter IP:
 read HOST
-cd chat && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../discord && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../films && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../game2048 && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../minecraft-server && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../minesweeper && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../server && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../timeline && scp -r dist/* pi@$HOST:/home/pi/server &
-cd ../vinyl && scp -r dist/* pi@$HOST:/home/pi/server &
+cd chat && scp -r dist/* net@$HOST:/home/net/server &
+cd ../discord && scp -r dist/* net@$HOST:/home/net/server &
+cd ../films && scp -r dist/* net@$HOST:/home/net/server &
+cd ../game2048 && scp -r dist/* net@$HOST:/home/net/server &
+cd ../minecraft-server && scp -r dist/* net@$HOST:/home/net/server &
+cd ../minesweeper && scp -r dist/* net@$HOST:/home/net/server &
+cd ../server && scp -r dist/* net@$HOST:/home/net/server &
+cd ../timeline && scp -r dist/* net@$HOST:/home/net/server &
+cd ../vinyl && scp -r dist/* net@$HOST:/home/net/server &
 echo Done.
 read
