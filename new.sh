11051d14136571c1a3143587edbcab43659f7d4d
Sorogon <sorogon.developer@gmail.com>
2019-12-26 19:41:48 +0100

    new projects now automatically added

--- new.sh
+++ new.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo Enter name:
 read name
+. ./projects
+PROJ="${PROJECTS} ${name}"
+echo "PROJECTS=\"$PROJ\"" > ./projects
 ng new $name --skipInstall=true --prefix=$name --routing=true --skipGit=true --style=css
 echo Installing dependencies...
 sudo npm i -g pnpm
