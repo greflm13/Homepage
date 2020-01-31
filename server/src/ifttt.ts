707f86cf18254d14c0f46d6088c8a499f15acaf8
Florian <greflm13@htl-kaindorf.ac.at>
2020-01-31 23:57:27 +0100

    ye

--- server/src/ifttt.ts
+++ server/src/ifttt.ts
@@ -13,36 +13,38 @@ _ifttt.post('/spotify', (req, res, next) => spotify(req, res, next));
 _ifttt.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
 
 function spotify(req: express.Request, res: express.Response, _next: express.NextFunction) {
-  const song: { title: string, subreddit: string } = req.body;
-  const spoti: { value1: string, value2: string, value3: string } = { value1: '', value2: '', value3: song.subreddit };
+  const song = req.body;
+  const spoti = { value1: '', value2: '', value3: song.subreddit };
 
-  song.title = song.title.split(regexp).join('');
-  spoti.value2 = song.title.slice(0, song.title.indexOf(' - '));
-  spoti.value1 = song.title.slice(song.title.indexOf(' - ') + 3);
+  if (song.title.includes(' - ')) {
+    song.title = song.title.split(regexp).join('');
+    spoti.value2 = song.title.slice(0, song.title.indexOf(' - '));
+    spoti.value1 = song.title.slice(song.title.indexOf(' - ') + 3);
 
-  const data = JSON.stringify(spoti);
-  const options = {
-    hostname: 'maker.ifttt.com',
-    port: 443,
-    path: '/trigger/spotify/with/key/fox0r0CxPR0h7fNmBKaFhKXE0JCOM1Vnf-_uCzaYyYF',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
+    const data = JSON.stringify(spoti);
+    const options = {
+      hostname: 'maker.ifttt.com',
+      port: 443,
+      path: '/trigger/spotify/with/key/fox0r0CxPR0h7fNmBKaFhKXE0JCOM1Vnf-_uCzaYyYF',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+      }
     }
-  }
-  const req_ = https.request(options, res_ => {
+    const request = https.request(options, response => {
 
-    res_.on('data', d => {
-      process.stdout.write(d)
+      response.on('data', d => {
+        // process.stdout.write(d)
+      })
     })
-  })
 
-  req_.on('error', error => {
-    console.error(error)
-  })
+    request.on('error', error => {
+      console.error(error)
+    })
 
-  req_.write(data)
-  req_.end()
+    request.write(data)
+    request.end()
 
-  res.sendStatus(200);
+    res.sendStatus(200);
+  }
 }
