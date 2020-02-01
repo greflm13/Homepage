7bdd718028089cc6922ae577490fed69dc382b11
Florian <greflm13@htl-kaindorf.ac.at>
2020-02-01 15:49:04 +0100

    restructured, now more users possible

--- server/src/ifttt.ts
+++ server/src/ifttt.ts
@@ -4,6 +4,21 @@ import https from 'https';
 
 export let _ifttt = express();
 const regexp = /\(.+\)|\[.+\]+/g
+const Users: User[] = [{
+  user: 'sorogon',
+  key: 'fox0r0CxPR0h7fNmBKaFhKXE0JCOM1Vnf-_uCzaYyYF',
+  subreddits: [
+    'progmetal',
+    'PowerMetal',
+    'symphonicmetal',
+    'progrockmusic',
+    'BlackMetal',
+    'Deathmetal',
+    'ClassicRock',
+    'retrowave',
+    'synthwave'
+  ]
+}]
 
 // _ifttt.use(express.static(path.join(__dirname, 'discord/')));
 // _ifttt.get(['/'], (_req, res, _next) => {
@@ -14,37 +29,58 @@ _ifttt.use('/node_modules', express.static(path.join(__dirname, '../node_modules
 
 function spotify(req: express.Request, res: express.Response, _next: express.NextFunction) {
   const song = req.body;
-  const spoti = { value1: '', value2: '', value3: song.subreddit };
+  const spoti: Data = { value1: '', value2: '', value3: song.subreddit };
 
   if (song.title.includes(' - ')) {
     song.title = song.title.split(regexp).join('');
     spoti.value2 = song.title.slice(0, song.title.indexOf(' - '));
     spoti.value1 = song.title.slice(song.title.indexOf(' - ') + 3);
 
-    const data = JSON.stringify(spoti);
-    const options = {
-      hostname: 'maker.ifttt.com',
-      port: 443,
-      path: '/trigger/spotify/with/key/fox0r0CxPR0h7fNmBKaFhKXE0JCOM1Vnf-_uCzaYyYF',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-      }
-    }
-    const request = https.request(options, response => {
-
-      response.on('data', _d => {
-        // process.stdout.write(_d)
-      })
+    Users.forEach(user => {
+      webhook(spoti, user);
     })
-
-    request.on('error', error => {
-      console.error(error)
-    })
-
-    request.write(data)
-    request.end()
-
-    res.sendStatus(200);
   }
+  res.sendStatus(200);
+}
+
+function webhook(dataa: Data, user: User) {
+  user.subreddits.forEach(subreddit => {
+    if (subreddit === dataa.value3) {
+      const data = JSON.stringify(dataa);
+      const options = {
+        hostname: 'maker.ifttt.com',
+        port: 443,
+        path: '/trigger/spotify/with/key/' + user.key,
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        }
+      }
+      const request = https.request(options, response => {
+
+        response.on('data', _d => {
+          // process.stdout.write(_d)
+        })
+      })
+
+      request.on('error', error => {
+        console.error(error)
+      })
+
+      request.write(data)
+      request.end()
+    }
+  })
+}
+
+interface User {
+  user: string,
+  key: string,
+  subreddits: string[]
+}
+
+interface Data {
+  value1: string,
+  value2: string,
+  value3: string
 }
