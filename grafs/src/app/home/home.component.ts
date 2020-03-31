5d37a7fb3a7c1adee88f5c4375cefcc363073955
Sorogon <florian.greistorfer@sorogon.eu>
2020-03-31 11:31:25 +0200

    yeet

--- grafs/src/app/home/home.component.ts
+++ grafs/src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   public cwidth: number;
   public graph: Graph = { points: [] };
   public graph2: Graph = { points: [] };
+  public graph3: Graph = { points: [] };
 
   constructor(private ngZone: NgZone) { }
 
@@ -24,6 +25,8 @@ export class HomeComponent implements OnInit, OnDestroy {
         x: i, y: (Math.pow(Math.E, (0.3 * i + 0.3)) + 0.6), stringx: i.toString()
       });
     }
+    this.graph3.points.push({ x: 0, y: 0.000, stringx: '0' });
+    this.graph3.points.push({ x: 1, y: 0.100, stringx: '1' });
     this.data();
     this.cheight = window.innerHeight;
     this.cwidth = window.innerWidth;
@@ -31,7 +34,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.ngZone.runOutsideAngular(() => this.loop());
     this.drawLoop = setInterval(() => {
       this.loop();
-    }, 100);
+    }, 1000);
   }
 
   ngOnDestroy() {
@@ -46,9 +49,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.cheight = window.innerHeight;
     this.cwidth = window.innerWidth;
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.drawCoordinateSystem('Date', 'Confirmed Cases');
-    this.drawGraph(this.graph, 2, 'red', false);
-    this.drawGraph(this.graph2, 2, 'blue', true);
+    // this.drawCoordinateSystem('Date', 'Confirmed Cases');
+    // this.drawGraph(this.graph, 2, 'red', false);
+    // this.drawGraph(this.graph2, 2, 'blue', true);
+    this.drawGraph(this.graph3, 2, 'green', true, 'x', 'y');
 
     // this.drawSquare(10, 100, 100, 'blue');
     // this.drawText('Blau', 110, 100, 'black');
@@ -84,7 +88,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.ctx.fillText(text, x, y);
   }
 
-  drawGraph(graph: Graph, width: number, color: string, decorations: boolean) {
+  drawGraph(graph: Graph, width: number, color: string, decorations: boolean, xAxisName: string, yAxisName: string) {
     const scaleyy = [];
     graph.points.forEach(point => {
       scaleyy.push(point.y);
@@ -115,17 +119,25 @@ export class HomeComponent implements OnInit, OnDestroy {
     });
     // cut off all but the first two digits then +1 then fill to original length with 0s
     let scaley = 1;
+    console.log(scaleyy);
     if (Math.round(scaleyy[0]).toString().length >= 2) {
       // tslint:disable-next-line: max-line-length radix
       scaley = parseInt((parseInt(Math.round(scaleyy[0]).toString().substr(0, 2)) + 1).toString() + '0'.repeat(Math.round(scaleyy[0]).toString().length - 2));
     } else if (scaleyy[0] < 1) {
       // tslint:disable-next-line: max-line-length radix
-      scaley = parseFloat('0.' + (parseInt(scaleyy[0].toString().substr(2, 2)) + 1).toString() + '0'.repeat(scaleyy[0].toString().length - 2));
+      scaley = parseFloat(scaleyy[0].toString().substr(0, scaleyy[0].toString().length - 1) + (parseInt(scaleyy[0].toString().substr(scaleyy[0].toString().length - 1, 1)) + 1).toString());
+    } else {
+      scaley = scaleyy[0] + 1;
     }
     if (this.log) {
-      // tslint:disable-next-line: radix
-      scaley = parseInt('1' + '0'.repeat(Math.round(scaleyy[0]).toString().length));
+      if (scaleyy[0] < 1) {
+
+      } else {
+        // tslint:disable-next-line: radix
+        scaley = parseInt('1' + '0'.repeat(Math.round(scaleyy[0]).toString().length));
+      }
     }
+    console.log(scaley);
     const scalex = Math.round(scalexx[0]);
     const originx = 100;
     const originy = this.cheight - 100;
@@ -166,6 +178,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
     this.ctx.stroke();
     this.drawScale(scaley, graph.points.length, graph);
+    this.drawCoordinateSystem(xAxisName, yAxisName);
   }
 
   drawScale(yAxis: number, steps: number, graph: Graph) {
@@ -199,7 +212,7 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.ctx.translate(90, (this.cheight - 100) - (this.cheight - 230) / 10 * i);
         this.ctx.rotate(0.5235988);
         this.ctx.textAlign = 'right';
-        this.ctx.fillText(Math.round(i * stepy).toString(), 0, 0);
+        this.ctx.fillText((i * stepy).toString().substr(0, yAxis.toString().length + 3), 0, 0);
         this.ctx.restore();
       }
     }
@@ -294,6 +307,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.graph.points.push({ x: 24, y: 1843, stringx: '20. Mrz' });
     this.graph.points.push({ x: 25, y: 2649, stringx: '21. Mrz' });
     this.graph.points.push({ x: 26, y: 3244, stringx: '22. Mrz' });
+    this.graph.points.push({ x: 27, y: 3611, stringx: '23. Mrz' });
   }
 
 }
