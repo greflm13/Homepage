a1310797a65c144c03b508b0a85d1e4d8fb858d6
greistof <florian.greistorfer@acp.at>
2020-08-24 09:52:22 +0200

    whoops

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . ./projects
 echo ""
-echo "installing server..."
+echo "updating server..."
 cd server && npm update
 for APP in $PROJECTS
 do
     echo ""
-    echo "installing $APP..."
+    echo "updating $APP..."
     cd ../$APP && ng update @angular/cli
 done
