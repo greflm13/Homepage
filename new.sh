2718bc4ac8b1ab3599a27e5e5203f359d2e0abb7
greistof <florian.greistorfer@acp.at>
2020-09-01 15:10:32 +0200

    touching server file

--- new.sh
+++ new.sh
@@ -4,6 +4,7 @@ read name
 . ./projects
 PROJ="${PROJECTS} ${name}"
 echo "PROJECTS=\"$PROJ\"" > ./projects
+touch ./server/src/$name.ts
 ng new $name --skipInstall=true --prefix=$name --routing=true --skipGit=true --style=css
 echo Installing dependencies...
 cd $name
