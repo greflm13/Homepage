bc1b18110cc9783e42a06fd461967d8964a87501
greistof <florian.greistorfer@acp.at>
2020-09-01 15:17:56 +0200

    template copy

--- new.sh
+++ new.sh
@@ -4,7 +4,8 @@ read name
 . ./projects
 PROJ="${PROJECTS} ${name}"
 echo "PROJECTS=\"$PROJ\"" > ./projects
-touch ./server/src/$name.ts
+cp ./template ./server/src/$name.ts
+sed -i s/template/$name/g ./server/src/$name.ts
 ng new $name --skipInstall=true --prefix=$name --routing=true --skipGit=true --style=css
 echo Installing dependencies...
 cd $name
