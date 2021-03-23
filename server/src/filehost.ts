7d06af325924243081d903d8bd61658b143f4d43
greflm13 <greflm13@htl-kaindorf.ac.at>
2021-03-23 19:30:44 +0100

    now recursive!

--- server/src/filehost.ts
+++ server/src/filehost.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import path from 'path';
+import serveStatic from 'serve-static';
 import util from 'util';
-const execFile = util.promisify(require('child_process').execFile);
+const exec = util.promisify(require('child_process').exec);
 
 export let _filehost = express();
 
 _filehost.get('**', (req, res, next) => refresh(req, res, next));
-_filehost.use(express.static(path.join(__dirname, 'filehost/')));
+// _filehost.use(express.static(path.join(__dirname, 'filehost/')));
+_filehost.use(serveStatic(path.join(__dirname, 'filehost/'), { 'index': ['index.html', 'index.htm'] }))
 _filehost.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
 
-async function refresh(_req: express.Request, _res: express.Response, next: express.NextFunction) {
-    await execFile(path.join(__dirname, 'filehost/refresh_filelist'));
+async function refresh(req: express.Request, _res: express.Response, next: express.NextFunction) {
+    if (req.path.endsWith('/')) {
+        await exec(path.join(__dirname, 'filehost/refresh_filelist ') + req.path);
+    }
     next();
 }
