a97179b386d6d05d9e7415b883a566ebe6602188
Flo Greistorfer <florian.greistorfer@k-business.com>
2024-03-18 10:13:40 +0100

    Revert "update angular to version 8"
    This reverts commit c4445e72151a9cd4192719e31ff630d1f35793d5.

--- minecraft-server/src/app/app.component.ts
+++ minecraft-server/src/app/app.component.ts
@@ -1,106 +1,77 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpService } from "./http.service";
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   public minecraft: Status = {
-    icon: "",
-    motd: { raw: "", clean: "", html: "" },
+    duration: 0,
+    error: '',
+    favicon: '',
+    motd: '',
+    last_online: '',
+    last_updated: '',
     online: false,
-    players: { max: 0, online: 0, list: [] },
-    version: { name_raw: "", name_clean: "", name_html: "", protocol: 0 },
-    eula_blocked: false,
-    expires_at: 0,
-    retrieved_at: 0,
-    host: "minecraft.sorogon.eu",
-    ip_address: "",
-    mods: [],
-    port: 25567,
-    plugins: [],
-    software: null,
-    srv_record: { host: "sorogon.eu", port: 25567 },
+    players: { max: 0, now: 0 },
+    server: { name: '', protocol: 0 },
+    status: ''
   };
   public neuland: Status = {
-    icon: "",
-    motd: { raw: "", clean: "", html: "" },
+    duration: 0,
+    error: '',
+    favicon: '',
+    motd: '',
+    last_online: '',
+    last_updated: '',
     online: false,
-    players: { max: 0, online: 0, list: [] },
-    version: { name_raw: "", name_clean: "", name_html: "", protocol: 0 },
-    eula_blocked: false,
-    expires_at: 0,
-    retrieved_at: 0,
-    host: "neuland.sorogon.eu",
-    ip_address: "",
-    mods: [],
-    port: 25566,
-    plugins: [],
-    software: null,
-    srv_record: { host: "sorogon.eu", port: 25566 },
+    players: { max: 0, now: 0 },
+    server: { name: '', protocol: 0 },
+    status: ''
   };
 
   constructor(private http: HttpService) {}
 
   ngOnInit() {
-    this.http.get("status/minecraft").then((res) => {
+    this.http.get('status/minecraft').then(res => {
       this.minecraft = res;
     });
     setInterval(() => {
-      this.http.get("status/minecraft").then((res) => {
+      this.http.get('status/minecraft').then(res => {
         this.minecraft = res;
       });
-    }, 10000);
-    this.http.get("status/neuland").then((res) => {
+    }, 1000);
+    this.http.get('status/neuland').then(res => {
       this.neuland = res;
     });
     setInterval(() => {
-      this.http.get("status/neuland").then((res) => {
+      this.http.get('status/neuland').then(res => {
         this.neuland = res;
       });
-    }, 10000);
+    }, 1000);
   }
 }
 
 interface Status {
+  status: string;
   online: boolean;
-  host: string;
-  port: number;
-  ip_address: string;
-  eula_blocked: boolean;
-  retrieved_at: number;
-  expires_at: number;
-  srv_record: SrvRecord;
-  version: Version;
+  motd: string;
+  favicon: string;
+  error: string;
   players: Players;
-  motd: Motd;
-  icon: string;
-  mods: any[];
-  software: any;
-  plugins: any[];
-}
-
-interface SrvRecord {
-  host: string;
-  port: number;
-}
-
-interface Version {
-  name_raw: string;
-  name_clean: string;
-  name_html: string;
-  protocol: number;
+  server: Server;
+  last_online: string;
+  last_updated: string;
+  duration: number;
 }
 
 interface Players {
-  online: number;
   max: number;
-  list: any[];
+  now: number;
 }
 
-interface Motd {
-  raw: string;
-  clean: string;
-  html: string;
+interface Server {
+  name: string;
+  protocol: number;
 }
