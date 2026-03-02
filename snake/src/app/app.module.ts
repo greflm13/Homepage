33d5d0764c381a672f8b6e7e9474ef941e61e8cf
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-02 11:15:42 +0100

    @angular/core migration - migration-http-providers
    Replace deprecated HTTP related modules with provider functions

--- snake/src/app/app.module.ts
+++ snake/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,18 +8,11 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HttpService } from './http.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    GameComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [HttpService, HttpClient],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        GameComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [HttpService, HttpClient, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
