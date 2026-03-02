70884d08323e2629923249fbde925ab9e30fcf19
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-02 14:17:05 +0100

    @nguniversal/express-engine migration - update-9 Update @nguniversal/express-engine to version 9.

--- chat/src/app/app.server.module.ts
+++ chat/src/app/app.server.module.ts
@@ -3,7 +3,6 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
   imports: [
