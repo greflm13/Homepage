4278e0c7b4931bff44ccfc72bebbe2061747d01e
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-03 08:02:51 +0100

    @angular/core migration - explicit-standalone-flag
    Updates non-standalone Directives, Component and Pipes to 'standalone:false' and removes 'standalone:true' from those who are standalone

--- timeline/src/app/app.component.ts
+++ timeline/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent implements OnInit {
   public windowWidth = window.innerWidth;
