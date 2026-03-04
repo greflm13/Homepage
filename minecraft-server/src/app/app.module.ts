4864ee80fa4bd7fd0be3466ffc1608f860c30f8c
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-04 14:58:24 +0100

    @angular/core migration - migration-http-providers
    Replace deprecated HTTP related modules with provider functions

--- minecraft-server/src/app/app.module.ts
+++ minecraft-server/src/app/app.module.ts
@@ -2,13 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpService } from './http.service';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [HttpService, HttpClient],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [HttpService, HttpClient, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
