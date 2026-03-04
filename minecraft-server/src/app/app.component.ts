e2210156cc85aca399040c2f208713b78a562e6f
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-04 15:00:52 +0100

    @angular/core migration - explicit-standalone-flag
    Updates non-standalone Directives, Component and Pipes to 'standalone:false' and removes 'standalone:true' from those who are standalone

--- minecraft-server/src/app/app.component.ts
+++ minecraft-server/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false
 })
 export class AppComponent implements OnInit {
   public minecraft: Status = {
