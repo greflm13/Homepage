9ab8326aab7d41b5fe97c9175763d8f44906be5e
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-03-04 15:12:01 +0100

    fixed

--- new.sh
+++ new.sh
@@ -12,11 +12,11 @@ PROJ="${PROJECTS} ${name}"
 echo "PROJECTS=\"$PROJ\"" >./projects
 cp ./template "./server/src/$name.ts"
 sed -i "s/template/$name/g" "./server/src/$name.ts"
-ng new "$name" --skip-install --prefix="$name" --routing --skip-git --style=css --ssr --ai-config=none --package-manager=npm
+ng new "$name" --skip-install --prefix="$name" --routing --skip-git --style=css --ai-config=none --package-manager=npm
 cat <<<"$(jq '. | select (.scripts) .scripts.start = "npm run build"' ./$name/package.json)" >./$name/package.json
 cat <<<"$(jq '. | select (.scripts) .scripts.restart = "scp -r dist/* $HOST:/home/pi/server"' ./$name/package.json)" >./$name/package.json
 cat <<<"$(jq '. | select (.scripts) .scripts.prerestart = "npm run build"' ./$name/package.json)" >./$name/package.json
-cat <<<"$(jq '. | select (.scripts) .scripts.build = "ng build --prod --build-optimizer --base-href=/'"$name"'/"' ./$name/package.json)" >./$name/package.json
+cat <<<"$(jq '. | select (.scripts) .scripts.build = "ng build --configuration production --base-href=/'"$name"'/"' ./$name/package.json)" >./$name/package.json
 cat <<<"$(jq '. | select (.scripts) .scripts.test = "ng build --watch --base-href=/'"$name"'/"' ./$name/package.json)" >./$name/package.json
 cat <<<"$(jq '.folders[.folders| length] |= . + {"path": "'"$name"'", "name": "'"$name"'" }' ./homepage.code-workspace)" >./homepage.code-workspace
 echo Installing dependencies...
